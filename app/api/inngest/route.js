e74e88e5d4fbadc0c24a5b9f24a6221f359c9fde
Khoa <khoaho120703@gmail.com>
2025-05-07 16:37:00 +0700

    Change location

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,12 +1,7 @@
 // File: /app/api/inngest/[[...inngest]]/route.ts
 
 import { serve } from "inngest/next";
-import {
-  inngest,
-  syncUserCreation,
-  syncUserDeletion,
-  syncUserUpdation,
-} from "@/config/inngest"; // Các hàm xử lý sự kiện đã định nghĩa
+import { inngest, syncUserCreation } from "@/config/inngest"; // Các hàm xử lý sự kiện đã định nghĩa
 
 // Đăng ký các hàm vào webhook server
 export const { GET, POST, PUT } = serve({
